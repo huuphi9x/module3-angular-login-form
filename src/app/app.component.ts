eb09b25d2d05d3be767c9e6515f21207960e031e
admin <huuphi9x@gmail.com>
2019-11-02 21:53:17 +0700

    add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'module3-angular-login-form';
+  title = 'angular-native-form-login';
 }
